9db6dae7c952ffbb438eae5a59db2ecacc7de18d
afonso <ajcerejeira@gmail.com>
2018-01-10 12:34:16 +0000

    Fixed #11

--- src/tests/T11.java
+++ src/tests/T11.java
@@ -24,15 +24,16 @@ public class T11 implements Test {
     public Map<String, Supplier<?>> indicators() {
         Map<String, Supplier<?>> indicators = new LinkedHashMap<>();
 
+        T1 t1 = new T1(this.transactions);
         T2 t2 = new T2(this.transactions);
         T5 t5 = new T5(this.transactions);
         T8 t8 = new T8(this.transactions);
         T10 t10 = new T10(this.transactions);
 
-        indicators.put("(1) byDateStream", t2::byDateStream);
-        indicators.put("(2) sortList", t5::sortList);
-        indicators.put("(3) biggestTransaction8", t8::biggestTransaction8);
-        indicators.put("(4) ivaStream", t10::ivaStream);
+        indicators.put("(1) sumStreamP", t1::sumStreamP);
+        indicators.put("(2) byDateStreamP", t2::byDateStreamP);
+        indicators.put("(3) sortList", t5::sortList);
+        indicators.put("(4) biggestTransaction8", t8::biggestTransaction8);
 
         return indicators;
     }
